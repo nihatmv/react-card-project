088d79e899290c00a3a4a4923dd583a1f92a00f3
Nihat Mammadli <nihatmammadli42@gmail.com>
2024-02-07 21:43:15 +0400

    styles

--- src/App.js
+++ src/App.js
@@ -9,28 +9,13 @@ function App() {
     setCount(count + number);
   };
 
-  const renderContent = () => {
-    if (count === 0) {
-      return (
-        <img
-          src={flameTowersImage}
-          className="image image-1"
-          alt="Flame Towers"
-          onClick={() => handleClick(1)}
-        />
-      );
-    } else {
-      return <div className="number">{count}</div>;
-    }
-  };
-
   return (
     <div className="App">
       <div className="container">
         <div className="box">
           <img
             src={flameTowersImage}
-            className="image image-1"
+            className="image"
             onClick={() => handleClick(1)}
           />
         </div>
@@ -78,6 +63,7 @@ function App() {
         </div>
       </div>
       <p>Count : {count}</p>
+      <div className="box box-script-1">1</div>
     </div>
   );
 }
